b8b41ed0945cbb83031836d89698ba6f68c203e1
Wojtek Wrona <w.wrona@selleo.com>
2016-08-30 19:17:37 +0200

    Set imageLabel and scrollArea properties

--- imageviewer.cpp
+++ imageviewer.cpp
@@ -7,8 +7,16 @@ ImageViewer::ImageViewer(QWidget *parent) :
 {
     ui->setupUi(this);
 
+    ui->imageLabel->setBackgroundRole(QPalette::Base);
+    ui->imageLabel->setSizePolicy(QSizePolicy::Ignored, QSizePolicy::Ignored);
+    ui->imageLabel->setScaledContents(true);
+
+    ui->scrollArea->setBackgroundRole(QPalette::Dark);
+
     QImage image("/Users/Wojtek/Pictures/Three-eyed_crow.jpg");
     ui->imageLabel->setPixmap(QPixmap::fromImage(image));
+
+    setWindowTitle(tr("Image Viewer"));
 }
 
 ImageViewer::~ImageViewer()
